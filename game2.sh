286a88260cbe53b0a889eb4e29be6e008c063469
takaoka <pinokions009@gmail.com>
2020-09-30 01:40:16 +0900

    enemy slide

--- game2.sh
+++ game2.sh
@@ -6,6 +6,8 @@ ENEMY_SYMBOL="X"
 EMPTY="0"
 ENEMY_ROWS=2
 ENEMY_COLS=10
+ENEMY_SLIDE_INTERVAL=5
+ENEMY_SLIDE_WIDTH=4
 ENEMY_FALL_INTERVAL=20
 INITIAL_X=10
 INITIAL_Y=20
@@ -38,6 +40,7 @@ while read -p "Press any key to start. To quit, press Ctrl+D." line; do
     x_missile=0
     y_missile=0
     declare -a enemies=()
+    x_enemy=0
     y_enemy=1
     
     enemy_line=""
@@ -48,9 +51,9 @@ while read -p "Press any key to start. To quit, press Ctrl+D." line; do
         enemies+=( $enemy_line )
     done
     cnt=0
-    enemy_cnt=0
     gameover=false
     point=0
+    is_enemy_sliding_right=true
     while true; do
         # Catch key input
         read -n 1 -t 0.001 c
@@ -79,9 +82,18 @@ while read -p "Press any key to start. To quit, press Ctrl+D." line; do
         fi
 
         # Move enemies
-        if [ $enemy_cnt -ge $ENEMY_FALL_INTERVAL ]; then
+        enemy_fall_cnt=$(( $cnt % $ENEMY_FALL_INTERVAL ))
+        enemy_slide_cnt=$(( $cnt % $ENEMY_SLIDE_INTERVAL ))
+        is_enemy_sliding_right=$(( 1 - ($cnt /($ENEMY_SLIDE_INTERVAL * $ENEMY_SLIDE_WIDTH)) % 2 ))
+        if [ $enemy_slide_cnt = 0 ]; then
+            if [ $is_enemy_sliding_right = 1 ]; then
+                x_enemy=$(( $x_enemy + 1 ))
+            else
+                x_enemy=$(( $x_enemy - 1 ))
+            fi
+        fi
+        if [ $enemy_fall_cnt = 0 ]; then
             y_enemy=$(( $y_enemy + 1 ))
-            enemy_cnt=0
         fi
 
         # Contact detection
@@ -116,12 +128,16 @@ while read -p "Press any key to start. To quit, press Ctrl+D." line; do
         fi
 
         #Draw characters
+        enemy_offset=""
+        for i in `seq 1 $x_enemy`; do
+            enemy_offset="0$enemy_offset"
+        done
         for i in `seq 1 $y`; do
             if [ $i = $y ]; then
                 print_symbol $CHARACTER $x
             elif [ $i -ge $y_enemy -a $i -lt $(( $y_enemy + $ENEMY_ROWS )) ]; then
                 j=$(( $i - $y_enemy ))
-                echo ${enemies[$j]} | sed -e 's/0/ /g'
+                echo "$enemy_offset${enemies[$j]}" | sed -e 's/0/ /g'
             elif [ $is_missile_flying = true -a $i = $y_missile ]; then
                 print_symbol $MISSILE_SYMBOL $x_missile
             else
@@ -132,12 +148,7 @@ while read -p "Press any key to start. To quit, press Ctrl+D." line; do
        
         sleep 0.1
 
-        enemy_cnt=$(( $enemy_cnt + 1 ))
-        
-        # cnt=$(( $cnt + 1 ))
-        # if [ $cnt -gt 10 ]; then
-        #     break
-        # fi
+        cnt=$(( $cnt + 1 ))
     done
 done
 
